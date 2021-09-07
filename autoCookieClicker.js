4a0472a3efab1cea6a286033332f1cde3130cf91
Tobias Raggl <tobias.raggl@yandex.com>
2021-09-07 02:01:45 +0200

    [INTIAL] Initial commit

---
+++ autoCookieClicker.js
@@ -0,0 +1,18 @@
+if(AutoClicker === undefined) var AutoClicker = {};
+if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+
+AutoClicker.launch = function(){
+	AutoClicker.interval = setInterval(Game.ClickCookie, 100);
+  AutoClicker.isLoaded = 1;
+}
+
+if(!AutoClicker.isLoaded){
+	if(CCSE && CCSE.isLoaded){
+		AutoClicker.launch();
+	}
+	else{
+		if(!CCSE) var CCSE = {};
+		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+		CCSE.postLoadHooks.push(AutoClicker.launch);
+	}
+}

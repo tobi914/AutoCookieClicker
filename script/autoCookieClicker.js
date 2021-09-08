ed52923f3066db91252916c6d7505365bd6c2163
Tobias Raggl <tobias.raggl@yandex.com>
2021-09-08 02:13:01 +0200

    [WIP] Initial toggle buttons

--- script/autoCookieClicker.js
+++ script/autoCookieClicker.js
@@ -20,9 +20,30 @@ AutoClicker.init = function() {
     }
   };
 
+  AutoClicker.createButtons = function() {
+    var container = document.createElement('div');
+    container.style.padding = '1rem';
+    container.style.backgroundColor = 'black';
+    container.style.position = 'absolute';
+    container.style.top: '0';
+    container.style.left: '0';
+    container.style.color: 'white';
+    var autoButton = document.createElement('button');
+    autoButton.innerHTML = 'Auto click';
+    var autoGoldenButton = document.createElement('button');
+    autoGoldenButton.innerHTML = 'Golden auto click';
+    container.appendChild(autoButton);
+    container.appendChild(autoGoldenButton);
+    var gameContainer = document.getElementById('game')
+    gameContainer.appendChild(container);
+    AutoClicker.ButtonContainer = container;
+    AutoClicker.AutoButton = autoButton;
+    AutoClicker.AutoGoldenButton = autoGoldenButton;
+  };
+
   window.setTimeout(() => {
-    window.setInterval(AutoClicker.ClickCookie, 25);
-    window.setInterval(AutoClicker.ClickGoldenCookie, 25);
+    AutoClicker.AutoClickInterval = window.setInterval(AutoClicker.ClickCookie, 25);
+    AutoClicker.GoldenClickInterval = window.setInterval(AutoClicker.ClickGoldenCookie, 25);
   }, 5000);
   Game.Notify(`AutoClicker mod loaded!`,'',[16,5]);
   AutoClicker.isLoaded = true;
